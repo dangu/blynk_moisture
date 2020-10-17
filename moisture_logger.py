8e6ae6307a127ac827fbbca274a328c74b2d559f
Daniel Gullberg <daniel_gullberg@hotmail.com>
2020-10-17 21:45:00 +0200

    Moisture logger with Arduino and DHT21

---
+++ moisture_logger.py
@@ -0,0 +1,43 @@
+import serial
+import logging
+import sys
+
+logger = logging.getLogger()
+
+
+
+
+class Moisture_logger():
+    def __init__(self):
+        """Init"""
+        self._ser=serial.Serial()
+        
+
+        
+    def start(self,port):
+        """Start"""
+        self._ser.port=port
+        self._ser.baudrate=9600
+        self._ser.open()
+        while(True):
+            logger.info(self._ser.readline().strip())
+
+def run():
+    """Run"""
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    handler = logging.FileHandler('log.txt', mode='w')
+    handler.setFormatter(formatter)
+    screen_handler = logging.StreamHandler(stream=sys.stdout)
+    screen_handler.setFormatter(formatter)
+
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    logger.addHandler(screen_handler)
+        
+    ml=Moisture_logger()
+    logger.info("Starting...")
+    ml.start(port="/dev/ttyUSB0")
+    
+if __name__=="__main__":
+    run()
